f78f67ace937d86c0702d31c17c2b551737c4860
Sara-PC\Sara <sara.ashour.89@gmail.com>
2017-09-09 22:59:34 -0700

    Initial commit

---
+++ app/src/main/java/com/education/capstone_stage2/view/fragment/DetailsFragment.java
@@ -0,0 +1,21 @@
+package com.education.capstone_stage2.view.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.education.capstone_stage2.R;
+
+
+public class DetailsFragment extends Fragment {
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+}
